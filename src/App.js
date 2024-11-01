642b6f13ccde8dc40936a5660cafa8d5de12e455
Huang, TzuChuan | Tommy | TWR <tzuchuan.huang@rakuten.com>
2024-11-01 18:25:34 +0800

    feat: add react-tab example

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import logo from "./logo.svg";
 import "./App.css";
+import cx from "classnames";
 import styles from "./page.module.scss";
+// https://github.com/reactjs/react-tabs/tree/v4.2.1
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+
+const TABS = [
+  { name: "全部優惠", description: "全部優惠的列表" },
+  { name: "買一送一", description: "This is a BOGO campaign" },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-
-      {/* https://github.com/reactjs/react-tabs/tree/v4.2.1 */}
-      <div className={styles.test}>{"content here"}</div>
+      <Tabs>
+        <TabList className={cx(styles.container, styles.tabList)}>
+          {TABS.map((TAB) => (
+            <Tab className={styles.tab}>{TAB.name}</Tab>
+          ))}
+        </TabList>
+        {TABS.map((TAB) => (
+          <TabPanel>{TAB.description}</TabPanel>
+        ))}
+      </Tabs>
     </div>
   );
 }
